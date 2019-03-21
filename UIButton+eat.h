b715e1ed81f8190111f497c66b7d1932e148a2db
MarkLiu <1144713192@qq.com>
2019-03-21 17:06:22 +0800

    0.1.1

---
+++ UIButton+eat.h
@@ -0,0 +1,16 @@
+//
+//  UIButton+eat.h
+//  AFNetworking
+//
+//  Created by MarkLiu on 2019/3/21.
+//
+
+#import <UIKit/UIKit.h>
+#import "AFNetworking.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIButton (eat)
+-(void)eat;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ UIButton+eat.m
@@ -0,0 +1,15 @@
+//
+//  UIButton+eat.m
+//  AFNetworking
+//
+//  Created by MarkLiu on 2019/3/21.
+//
+
+#import "UIButton+eat.h"
+
+@implementation UIButton (eat)
+-(void)eat
+{
+    AFHTTPSessionManager * maa = [AFHTTPSessionManager manager];
+}
+@end
